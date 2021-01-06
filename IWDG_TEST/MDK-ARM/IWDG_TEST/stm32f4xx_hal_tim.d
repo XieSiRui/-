3abc2fe40d7363b77a056e8110c90f5b8330931e
XieSiRui <75836732+XieSiRui@users.noreply.github.com>
2021-01-06 15:59:41 +0800

    看门狗
    基于CUBEMX自制看门狗（没有使用HAL库看门狗）

---
+++ IWDG_TEST/MDK-ARM/IWDG_TEST/stm32f4xx_hal_tim.d
@@ -0,0 +1,32 @@
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Core/Inc/stm32f4xx_hal_conf.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f405xx.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Include/core_cm4.h
+iwdg_test\stm32f4xx_hal_tim.o: F:\Keil 5\ARM\ARMCC\Bin\..\include\stdint.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Include/cmsis_version.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Include/mpu_armv7.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+iwdg_test\stm32f4xx_hal_tim.o: F:\Keil 5\ARM\ARMCC\Bin\..\include\stddef.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_exti.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_iwdg.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h
+iwdg_test\stm32f4xx_hal_tim.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h
---
+++ IWDG_TEST/MDK-ARM/RTE/_IWDG_TEST/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'IWDG_TEST' 
+ * Target:  'IWDG_TEST' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
